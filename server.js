c76f97ca790a337493a545cdf2bd84af240d6ac1
LarisseLima <larisse.lima2@gmail.com>
2020-07-24 15:38:49 -0300

    Criação de registro

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
+app.use(express.json());
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi',
+mongoose.connect("mongodb://localhost:27017/nodeapi",
     { useUnifiedTopology: true, useNewUrlParser: true }
 );
 requireDir("./src/models");
--- src/models/controllers/ProductController.js
+++ src/models/controllers/ProductController.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Product = mongoose.model('Product');
+const Product = mongoose.model("Product");
 
 module.exports = {
     async index(req, res) {
         const products = await Product.find();
 
-        return res.json(process);
+        return res.json(products);
 
     },
 
+    async store(req, res) {
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    }
 };
