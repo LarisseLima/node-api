283807221a82796807545df4bc2c49ef0696dbb9
Larisse <larisse.lima2@gmail.com>
2020-09-14 15:35:28 -0300

    Finaliza CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,41 +1,34 @@
 const mongoose = require("mongoose");
-
 const Product = mongoose.model("Product");
 
 module.exports = {
+    // Visualizar
     async index(req, res) {
         const products = await Product.find();
-
         return res.json(products);
 
     },
-
+    // Detalhar por ID
     async show(req, res) {
         const product = await Product.findById(req.params.id);
-
         return res.json(product);
 
     },
-
+    // Criar
     async store(req, res) {
         const product = await Product.create(req.body);
-
         return res.json(product);
     },
-
+    // Atualizar
     async update(req, res) {
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
             new: true
         });
-
         return res.json(product);
     },
-
+    // Deletar
     async destroy(req, res) {
-        const product = await Product.create(req.body);
-
-        return res.json(product);
-    },
-
-
+        const product = await Product.findByIdAndRemove(req.params.id);
+        return res.send();
+    }
 };
