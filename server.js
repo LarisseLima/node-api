69ff4904177b7a996ca613f6c061b698c9e429fa
Larisse <larisse.lima2@gmail.com>
2020-09-14 15:56:39 -0300

    Adicionando CORS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Importar
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express(); //Executar a função
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi",
